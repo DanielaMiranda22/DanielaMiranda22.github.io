5401732f7242bf05a7ca68768f2afd42ee2b6fb2
Daniela G. Miranda <140209051+DanielaMiranda22@users.noreply.github.com>
2023-08-08 07:26:15 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//Función mostrar y ocultar info //
+
+function muestra_oculta(id){
+    let div = document.getElementById(id);
+    if (div.style.display = "flex"){
+        div.style.display == "none";
+    }
+    else {
+        div.style.display ="none";
+    }
+}
